4151cb4e09c2600663578f7c67648740e6a9d3e9
SpyderGamer <hnoussa244@gmail.com>
2022-07-15 16:40:35 +0400

    File Uploaded

---
+++ Home.js
@@ -0,0 +1,12 @@
+// This is the Javascript file we are going to use for the actual code.
+// You can view the console in your broswer by running the Index.html file and Right Click the Page >> Inspect Element >> Console.
+// You can also view the console with "Ctrl + Shift + I"
+
+console.log("Hello World!");
+alert("This is an Alert.");
+
+let stringVariable = "This is a String Variable.";
+console.log(stringVariable);
+
+let intVariable = 100;
+console.log(intVariable);
