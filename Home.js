62d84224276d6b72b492a8093189daf92265a9ca
SpyderGamer <hnoussa244@gmail.com>
2022-08-21 12:13:02 +0400

    Index.html can now run on Github Pages.
    Index.html >> index.html

--- Home.js
+++ Home.js
@@ -1,6 +1,6 @@
 // This is the Javascript file we are going to use for the actual code.
-// You can view the console in your broswer by running the Index.html file and Right Click the Page >> Inspect Element >> Console.
-// You can also view the console with "Ctrl + Shift + I"
+// You can view the console in your broswer by running the `index.html` file and Right Click the Page >> Inspect Element >> Console.
+// You can also view the console with "Ctrl + Shift + I".
 
 console.log("Hello World!");
 alert("This is an Alert.");
